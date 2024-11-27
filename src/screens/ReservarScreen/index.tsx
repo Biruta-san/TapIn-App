94977b2426061019705a3e9a9b0a4914a7b742f7
biruta-san <ryanhenriq2019@gmail.com>
2024-11-27 18:52:30 -0300

    Implementações na tela de hotel

--- src/screens/ReservarScreen/index.tsx
+++ src/screens/ReservarScreen/index.tsx
@@ -26,12 +26,76 @@ import {
   HOTEL_ROUTE,
 } from '../../shared/apiroutes';
 import {userContext} from '../../shared/context/UserProvider';
+import {Spinner} from '@ui-kitten/components';
+import Layout from '../../shared/components/Layouts/Layout';
 
 type ReservarScreenProps = {
   route: RouteProp<{params: {hotelId: number}}, 'params'>;
   navigation: {
     navigate: (screen: string) => void;
   };
+};
+
+interface SectionProps {
+  loading: boolean;
+  quando: {startDate?: Date; endDate?: Date};
+  setQuando: (range: {startDate?: Date; endDate?: Date}) => void;
+  quantasPessoas?: string;
+  setQuantasPessoas: (value: string) => void;
+  quartoOptions: Option[];
+  setQuarto: (value: number) => void;
+  saveReservation: () => void;
+}
+
+const SectionContent = ({
+  loading,
+  quando,
+  setQuando,
+  quantasPessoas,
+  setQuantasPessoas,
+  quartoOptions,
+  setQuarto,
+  saveReservation,
+}: SectionProps) => {
+  if (loading) {
+    return (
+      <>
+        <Spinner />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <RangeDatepicker
+          range={quando}
+          onSelect={nextRange => setQuando(nextRange)}
+          label={'Período da viagem'}
+          placeholder={'Selecione o período'}
+          mb={5}
+        />
+        <NumericInput
+          value={quantasPessoas}
+          onChange={value => setQuantasPessoas(value)}
+          w={'100%'}
+          placeholder={'Quantidade de pessoas'}
+          label={'Informe a quantidade de pessoas'}
+          mb={5}
+        />
+        <Selection
+          label={'Selecione o quarto'}
+          placeholder={'Selecione o quarto'}
+          w={'100%'}
+          options={quartoOptions}
+          setValue={value => setQuarto(Array.isArray(value) ? value[0] : value)}
+        />
+        <BaseButton mb={5} w={'100%'} onPress={saveReservation}>
+          <Text fontWeight="bold" color="white">
+            Agendar
+          </Text>
+        </BaseButton>
+      </>
+    );
+  }
 };
 
 const ReservarScreen: React.FC<ReservarScreenProps> = ({route, navigation}) => {
@@ -41,28 +105,32 @@ const ReservarScreen: React.FC<ReservarScreenProps> = ({route, navigation}) => {
   const [quantasPessoas, setQuantasPessoas] = useState<string>();
   const [quarto, setQuarto] = useState<number>();
   const [quartoOptions, setQuartoOptions] = useState<Option[]>([]);
-
-  const fotos: string[] = [
-    'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/40/e5/50/20190118-193234-largejpg.jpg',
-    'https://www.civitatis.com/blog/wp-content/uploads/2022/11/downtown-orlando-florida.jpg',
-  ];
+  const [loading, setLoading] = useState<boolean>(false);
+  const [agendarLoading, setAgendarLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      if (hotelId) {
-        const result = await getDataByIdApi<Hotel>(HOTEL_ROUTE, hotelId);
-        setDadosHotel(result.data);
-        const apiOptions = await getDataByIdApi<ListHotelQuarto[]>(
-          HOTEL_QUARTO_LIST_ROUTE,
-          hotelId,
-        );
-        if (apiOptions.status >= 200 && apiOptions.status < 300) {
-          const options: Option[] = apiOptions.data.map(q => ({
-            value: q.id,
-            title: q.numero.toString(),
-          }));
-          setQuartoOptions(options);
+      try {
+        setLoading(true);
+        if (hotelId) {
+          const result = await getDataByIdApi<Hotel>(HOTEL_ROUTE, hotelId);
+          setDadosHotel(result.data);
+          const apiOptions = await getDataByIdApi<ListHotelQuarto[]>(
+            HOTEL_QUARTO_LIST_ROUTE,
+            hotelId,
+          );
+          if (apiOptions.status >= 200 && apiOptions.status < 300) {
+            const options: Option[] = apiOptions.data.map(q => ({
+              value: q.id,
+              title: q.numero.toString(),
+            }));
+            setQuartoOptions(options);
+          }
         }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchData();
@@ -70,6 +138,7 @@ const ReservarScreen: React.FC<ReservarScreenProps> = ({route, navigation}) => {
 
   const saveReservation = async () => {
     try {
+      setAgendarLoading(true);
       const response = await postDataApi<HotelQuartoAgendamento>(
         HOTEL_AGENDAR_ROUTE,
         {
@@ -84,6 +153,8 @@ const ReservarScreen: React.FC<ReservarScreenProps> = ({route, navigation}) => {
       }
     } catch (error) {
       console.error(error);
+    } finally {
+      setAgendarLoading(false);
     }
   };
 
@@ -94,60 +165,52 @@ const ReservarScreen: React.FC<ReservarScreenProps> = ({route, navigation}) => {
     {
       title: 'Realizar reserva',
       content: (
-        <>
-          <RangeDatepicker
-            range={quando}
-            onSelect={nextRange => setQuando(nextRange)}
-            label={'Período da viagem'}
-            placeholder={'Selecione o período'}
-            mb={5}
-          />
-          <NumericInput
-            value={quantasPessoas}
-            onChange={value => setQuantasPessoas(value)}
-            w={'100%'}
-            placeholder={'Quantidade de pessoas'}
-            label={'Informe a quantidade de pessoas'}
-            mb={5}
-          />
-          <Selection
-            label={'Selecione o quarto'}
-            placeholder={'Selecione o quarto'}
-            w={'100%'}
-            options={quartoOptions}
-            setValue={value =>
-              setQuarto(Array.isArray(value) ? value[0] : value)
-            }
-          />
-          <BaseButton mb={5} w={'100%'} onPress={saveReservation}>
-            <Text fontWeight="bold" color="white">
-              Agendar
-            </Text>
-          </BaseButton>
-        </>
+        <SectionContent
+          loading={agendarLoading}
+          quando={quando}
+          setQuando={setQuando}
+          quantasPessoas={quantasPessoas}
+          setQuantasPessoas={setQuantasPessoas}
+          quartoOptions={quartoOptions}
+          setQuarto={setQuarto}
+          saveReservation={saveReservation}
+        />
       ),
     },
   ];
 
+  if (loading) {
+    return (
+      <Layout bg="white">
+        <Spinner size="giant" />
+      </Layout>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <ImageSlider
-        images={fotos}
-        imageHeight={250}
-        dotSize={10}
-        dotColor={retrieveColorString(styleTypeEnums.PRIMARY, weightEnums[700])}
-        activeDotColor={retrieveColorString(
-          styleTypeEnums.PRIMARY,
-          weightEnums[700],
-        )}
-        showNavigationButtons={false}
-        showIndicatorDots={true}
-        imageLabel={false}
-        extrapolate="clamp"
-        autoSlideInterval={10000}
-        containerStyle={styles.container}
-        radius={5}
-      />
+      {dadosHotel?.imagens && dadosHotel?.imagens.length > 0 && (
+        <ImageSlider
+          images={dadosHotel?.imagens}
+          imageHeight={250}
+          dotSize={10}
+          dotColor={retrieveColorString(
+            styleTypeEnums.PRIMARY,
+            weightEnums[700],
+          )}
+          activeDotColor={retrieveColorString(
+            styleTypeEnums.PRIMARY,
+            weightEnums[700],
+          )}
+          showNavigationButtons={false}
+          showIndicatorDots={true}
+          imageLabel={false}
+          extrapolate="clamp"
+          autoSlideInterval={10000}
+          containerStyle={styles.container}
+          radius={5}
+        />
+      )}
       <View style={styles.detailsContainer}>
         <Text category={'h1'} style={styles.hotelName}>
           {`${dadosHotel?.nome}`}
